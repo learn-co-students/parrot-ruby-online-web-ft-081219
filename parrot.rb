1538ff8038435f97c2b2e89e93b4916c88e1e804
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-12 18:05:59 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,22 @@
+def parrot
+  phrase = "Squawk!"
+  puts phrase
+end
+
+def parrot (phrase = "Squawk!")
+  return parrot
+end
+
+def parrot(phrase) 
+  puts "#{phrase}"
+end 
+
+def parrot(phrase) 
+  return "#{phrase}"
+end
+
+
+
+
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
